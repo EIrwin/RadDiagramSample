04282a2339d6abaf1919d00d8127f6da86a6b05e
Eric Irwin <Eric.Irwin1124@gmail.com>
2014-09-30 19:00:03 -0700

    Added ControlTemplateSelector Placeholder

---
+++ RadDiagramSample/Views/ControlTemplateSelector.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RadDiagramSample.Views
+{
+    public class ControlTemplateSelector:DataTemplateSelector
+    {
+        public override System.Windows.DataTemplate SelectTemplate(object item, System.Windows.DependencyObject container)
+        {
+            //Retrieve 'announced' DataTemplate based on
+            //state informtion from 'item' parameter
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
