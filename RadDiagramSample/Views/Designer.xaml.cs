b5b770ab33b0b30d593fb9fa5adbef7c0c2202d3
Eric Irwin <Eric.Irwin1124@gmail.com>
2014-10-13 14:50:11 -0700

    Added Designer.xaml.cs

---
+++ RadDiagramSample/Views/Designer.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+using RadDiagramSample.ViewModels;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Controls.Diagrams;
+using Telerik.Windows.Diagrams.Core;
+
+namespace RadDiagramSample.Views
+{
+    /// <summary>
+    /// Interaction logic for Designer.xaml
+    /// </summary>
+    public partial class Designer : RadDiagram
+    {
+        protected DesignerViewModel ViewModel;
+
+        private int Test { get; set; }
+        public Designer()
+        {
+
+            InitializeComponent();
+
+            ViewModel = new DesignerViewModel();
+
+            DataContext = ViewModel;
+        }
+
+
+        private void Diagram_ConnectionManipulationCompleted(object sender, ManipulationRoutedEventArgs e)
+        {
+            if (e.Shape == null)
+            {
+                
+                if (Diagram.Connections.Contains(e.ManipulationPoint.Connection))
+                {
+                    Diagram.RemoveConnection(e.ManipulationPoint.Connection);
+                    ViewModel.RemoveConnection(e.Connection.Id);
+                }
+
+                e.Handled = true;
+            }
+            else
+            {
+                if (!Diagram.Connections.Any(p => p.Target == e.Connection.Target && p.Source == e.Connection.Source))
+                {
+                    //Check if connection already exists in 
+                    ConnectionViewModel model = new ConnectionViewModel()
+                        {
+                            Id = e.Connection.Id,
+                            Source =
+                                ((ControlView) e.ManipulationPoint.Connection.Source).DataContext as ControlViewModel,
+                            SourceCapType = CapType.None,
+                            Target = ((ControlView) e.Shape).DataContext as ControlViewModel,
+                            TargetCapType = CapType.Arrow1,
+                            Position = e.ManipulationPoint.Position,
+                        };
+
+                    ViewModel.AddConnection(model);
+                }
+            }
+        }
+
+        private void Diagram_PreviewDrop(object sender, System.Windows.DragEventArgs e)
+        {
+            ListBoxItem listBoxItem = (ListBoxItem)e.Data.GetData(e.Data.GetFormats()[0]);
+            ListBoxViewModel listBoxViewModel = (ListBoxViewModel)listBoxItem.DataContext;
+
+            ControlViewModel model = ControlViewModelFactory.Create(listBoxViewModel.ComponentType);
+            model.Position = e.GetPosition(this.Diagram);
+
+            ControlView view = ControlViewFactory.Create(listBoxViewModel.ViewType);
+            view.DataContext = model;
+
+            Diagram.AddShape(view);
+
+            ViewModel.AddControl(model);
+        }
+    }
+}
