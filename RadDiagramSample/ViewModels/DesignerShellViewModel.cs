87a3ede1f7174a348469146cd6374c3b8c627198
Eric Irwin <Eric.Irwin1124@gmail.com>
2014-10-14 19:40:43 -0700

    Added DesignerShellViewModel and changed name of parent view to DesignerShell

---
+++ RadDiagramSample/ViewModels/DesignerShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Telerik.Windows.Controls;
+
+namespace RadDiagramSample.ViewModels
+{
+    public class DesignerShellViewModel:ViewModelBase
+    {
+        private List<DesignerViewModel> _designers;
+
+        public DesignerShellViewModel()
+        {
+            _designers = new List<DesignerViewModel>();
+        }
+    }
+}
---
+++ RadDiagramSample/Views/DesignerShell.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using RadDiagramSample.ViewModels;
+
+namespace RadDiagramSample.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class DesignerShell : Window
+    {
+        public DesignerShellViewModel ViewModel;
+
+        public DesignerShell()
+        {
+            InitializeComponent();
+
+            ViewModel = new DesignerShellViewModel();
+
+            DataContext = ViewModel;
+
+            //Initialize DesignerShellViewModel here
+        }
+
+        private void Generate_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
